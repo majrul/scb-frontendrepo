d8be5c074f2dde9da3e57b68c6ca8691d622bf3e
Majrul Ansari <majrul@gmail.com>
2023-08-30 12:32:57 +0530

    updated with useEffect hook

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import axios from "axios";
 
@@ -6,21 +6,35 @@ function Profile() {
 
     const params = useParams();
 
-    const [userData,setUserData] = useState({});
+    const [userData,setUserData] = useState({
+        id: '',
+        name: '',
+        profilePic: '',
+        address: {
+            city: '',
+            pincode: ''
+        }
+    });
 
-    axios.get(`http://localhost:8080/customer/fetch/${params.id}`).then((response => {
+    //useEffect(() => { }, [stateVariable]); the side effect will run whenever the state changes
+    //useEffect(() => {}, []); //use this technique if in your component you want some code to run only once
+
+    useEffect(() => {
+        axios.get(`http://localhost:8080/customer/fetch/${params.id}`).then((response => {
             //console.log(response);
             //console.log(response.data);
             setUserData(response.data);
-    }))
+        }))
+    }, [params.id])
+   
 
     return (
-        <div>
+        (userData && <div>
             ID : {userData.id} <br />
             Name : {userData.name} <br />
             City : {userData.address.city} <br />
             ProfilePic : <img src={`http://localhost:8080/customer/fetch/profilePic/${userData.id}`} />
-        </div>
+        </div>)
     )
 }
 
